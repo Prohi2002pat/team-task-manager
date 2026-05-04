289bf470b92f9591c16b98092f256fa6f1882cc5
ROHIT SATISH PATIL <126314851+Prohi2002pat@users.noreply.github.com>
2026-05-04 23:35:01 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://team-task-manager-production-b778.up.railway.app/api",
+  baseURL: "https://team-task-manager-production-b778.up.railway.app/api",
 });
 
 export default API;
